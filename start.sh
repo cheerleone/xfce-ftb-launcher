192f27d849b6b5bdbf05ce5d123a5268532baf2c
cheerleone <cheerleone@gmail.com>
2014-11-07 04:24:21 -0500

    Update start.sh
    changed the method of detecting launcher load status from a cheap hack to something more reasonable: a log entry.

--- start.sh
+++ start.sh
@@ -5,95 +5,110 @@
 # License: GPL-V2.0
 
 # This script disables XFCE compositing prior to executing the FTB Launcher,
-# then when FTB minecraft JVM is running, compositing is enabled.
+# we redirect the launcher output to a logfile and parse for a string that
+# indicates the launcher has fully loaded, then wm compositing is re-enabled.
 
 # This addresses a recent bug between the FTB Launcher and XFCE that would leave
 # an artifact on the upper left corner of the screen until FTB is closed.
-
-# Testing is done on the minecraft JVM and not the launcher due to the GUI delay
-# between detectecting the launcher, and the launcher fully loading.
 
 # It was considered to respect the compositor off/false state prior to running,
 # however, it was not fathomable why someone would use this script when
 # their choice was to have compositing off.
 
-# Instances where this script does not apply:
-#   Running multiple concurrent FTB sessions.
-
-# Known Problems:
-#   Switching viewports while the client is loading sometimes allows the
-#   artifact to show again.
-
 # -- Globals -------------------------------------------------------------------
 
-    jvmIsRunning=false # (Java Virtual Macine is running - minecraft client)
-    compositorState=true # on/off state tracking.
+    launcherLoaded=false # launcher state tracking.
 
 # -- Constants -----------------------------------------------------------------
 
-    readonly version="1.0.4.beta" # major.minor.point.stage
+    readonly version="1.1.0.beta" # major.minor.point.stage
     readonly workingDir=$HOME/Games/ftb # FTB Launcher Location
     readonly ftbFileName=FTB_Launcher.jar # FTB jar file to launch
+    readonly launcherLogFile=launcher.log # FTB Launcher - per launch log
     
-    # note, advanced optoins for the JVM are set in the Launcher.
+    # note, advanced optoins for the minecraft client are set in the Launcher.
     
 # -- Functions -----------------------------------------------------------------
 
 function compositorOn() {
     # enable xfce compositing.
     xfconf-query -c xfwm4 -p /general/use_compositing -s true
-    compositorState=true
 }
 
 function compositorOff() {
     # disable xfce compositing.
     xfconf-query -c xfwm4 -p /general/use_compositing -s false
-    compositorState=false
 }
 
-function checkJVM() {
-    # quetly test if both java is running and that java is minecraft via 
-    # grep exit code. this identifies the minecraft client and not the launcher.
-    ps ax | grep java | grep minecraft | grep -v grep > /dev/null
-    if [ "$?" = "0" ]; then
-        jvmIsRunning=true
+function deleteOldLog() {
+    # delete previous launcher logfile if it exists.
+    if [ -f $workingDir/$launcherLogFile ]; then
+        echo "[$0] deleting log"
+        rm $workingDir/$launcherLogFile > /dev/null
     fi
 }
 
-function enableCompositorOnJVM() {    
-    # check once per second if the JVM is running.
-    while [ $jvmIsRunning = false ]; do        
-        checkJVM;
-        sleep 1;
+function checkLog() {
+    # wait for logfile to exist.
+    until [ -f $workingDir/$launcherLogFile ]; do
+        sleep 0.5;
+    done    
+
+    # check if logfile contains specified text that indicates the launcher
+    # has loaded.
+    cat $workingDir/$launcherLogFile | grep "Launcher Startup took" > /dev/null
+    if [ "$?" = "0" ]; then
+        launcherLoaded=true
+    fi
+}
+
+function enableCompositorOnLoad() {    
+    # check once per 0.5 seconds if FTB Launcher has loaded.
+    echo "[$0] waiting for launcher to fully load"
+    echo -n "[$0] "
+    while [ $launcherLoaded = false ]; do        
+        checkLog;
+        echo -n .
+        sleep 0.5;
     done
     
-    # when JVM is running re-enable compositor.
+    # Now that FTB is running, notify user, then re-enable compositor.
+    echo -e "\n[$0] launcher loaded"
+
+    # re-enable compositing
+    echo "[$0] enabling compositor"
     compositorOn;
 }
 
 # -- Main Function -------------------------------------------------------------
 
 function main() {  
-    # disable xfce compositing
+    # delete old launcher logfile.
+    deleteOldLog;
+    
+    # disable xfce compositing.
+    echo "[$0] disabling compositor"
     compositorOff;
     
-    # fork into backdround, disable, then re-enable xfce compositing when JVM
+    # fork into backdround, disable, then re-enable xfce compositing when FTB
     # is detected as running.
-    enableCompositorOnJVM &
-    # set working directory for FTB
+    enableCompositorOnLoad &
+    
+    # set working directory for FTB.
     cd $workingDir
     
-    # start FTB launcher
-    java -jar $ftbFileName >> $workingDir/launcher.log
+    # start FTB launcher.
+    echo "[$0] starting launcher"
+    java -jar $ftbFileName > $workingDir/$launcherLogFile
     
-    # test if compositor is off, re-enable if off. This state exists when the
-    # launcher is terminated by the user, leaving the forked process running.
-    if [ $compositorState = false ]; then
-        compositorOn;
+    # force compositor on. State could exist if Launcher failed to fully load.
+    echo "[$0] force enabling compositor ;P"
+    compositorOn;
 
-        # clean up orphaned process
-        pkill -P $$
-    fi
+    # clean up orphaned process.
+    pkill -P $$
+
+    echo -e "[$0] exit\n"
 }
 
 # -- Main Call -----------------------------------------------------------------
