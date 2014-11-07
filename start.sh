fca899712c52defc63a82310442a425744e1e56d
cheerleone <cheerleone@gmail.com>
2014-11-07 00:38:11 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # XFCE - FTB Artifact Fix (XFart.f?)
-# Copyright 2014 CheerLeone.
+# Copyright 2014 RWM.
 # License: GPL-V2.0
 
 # This script disables XFCE compositing prior to executing the FTB Launcher,
@@ -20,6 +20,10 @@
 # Instances where this script does not apply:
 #   Running multiple concurrent FTB sessions.
 
+# Known Problems:
+#   Switching viewports while the client is loading sometimes allows the
+#   artifact to show again.
+
 # -- Globals -------------------------------------------------------------------
 
     jvmIsRunning=false # (Java Virtual Macine is running - minecraft client)
@@ -27,7 +31,7 @@
 
 # -- Constants -----------------------------------------------------------------
 
-    readonly version="1.0.3.beta" # major.minor.point.stage
+    readonly version="1.0.4.beta" # major.minor.point.stage
     readonly workingDir=$HOME/Games/ftb # FTB Launcher Location
     readonly ftbFileName=FTB_Launcher.jar # FTB jar file to launch
     
@@ -48,9 +52,9 @@ function compositorOff() {
 }
 
 function checkJVM() {
-    # quetly test if both JVM is running and that JVM is FTB via grep exit code
-    # this identifies the minecraft client and not the launcher.
-    ps ax | grep "/usr/lib/jvm/java" | grep "ftb" | grep -v grep > /dev/null
+    # quetly test if both java is running and that java is minecraft via 
+    # grep exit code. this identifies the minecraft client and not the launcher.
+    ps ax | grep java | grep minecraft | grep -v grep > /dev/null
     if [ "$?" = "0" ]; then
         jvmIsRunning=true
     fi
@@ -76,17 +80,17 @@ function main() {
     # fork into backdround, disable, then re-enable xfce compositing when JVM
     # is detected as running.
     enableCompositorOnJVM &
-    
     # set working directory for FTB
     cd $workingDir
     
     # start FTB launcher
-    java -jar $ftbFileName > /dev/null
-
+    java -jar $ftbFileName >> $workingDir/launcher.log
+    
     # test if compositor is off, re-enable if off. This state exists when the
     # launcher is terminated by the user, leaving the forked process running.
     if [ $compositorState = false ]; then
         compositorOn;
+
         # clean up orphaned process
         pkill -P $$
     fi
